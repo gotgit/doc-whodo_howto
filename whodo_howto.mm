9226b3441224582ff39a745211e706bbca69a71e
Jiang Xin <worldhello.net@gmail.com>
2006-05-07 09:03:49 +0000

    整理: eol-style, mime-types

---
+++ whodo_howto.mm
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.8.0">
+<!-- This .mm file is CVS/SVN friendly, also has better Chinese character support. Contribute by http://www.WorldHello.net, orignal FreeMind can be found at http://freemind.sourceforge.net -->
+<node ID="Freemind_Link_1738528425" 
+	TEXT="WHODO Howto">
+<node FOLDED="true" ID="_" POSITION="right" 
+	TEXT="WHODO 是什么？">
+<node ID="Freemind_Link_173292206" 
+	TEXT="WHODO 即“WorldHello Open Document Origin”， “WorldHello 开放文档之源” 的缩写。&#xa;是使用 Freemind + DocBook + SVN 进行技术文档撰写的开放平台，这里的所有文档遵从GNU 的自由文档许可证(Free Document License)的条款，人人皆可提交更改。"/>
+<node ID="Freemind_Link_1178194788" 
+	TEXT="WHODO = Freemind + DocBook + CVS/SVN"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_1263641711" POSITION="right" 
+	TEXT="Wiki 难道不够么？">
+<node ID="Freemind_Link_1168664395" 
+	TEXT="Wiki 又作维客，是协同著作的伟大实践。Wiki 可以作为团队的知识库支撑系统，例如 WHODO 就用 Wiki 技术进行主页的搭建。"/>
+<node ID="Freemind_Link_1155624865" 
+	TEXT="难道 Wiki 不够么，还要使用 DocBook、Freemind 么？"/>
+<node ID="Freemind_Link_386104319" 
+	TEXT="有这么一个比喻，当你的网络出现故障，一、你记得访问过某个 Wiki 网站，其中有关于网络故障诊断的详细资料；二、有一个 DocBook 撰写的网络故障诊断的电子书你看过并保存在硬盘中。&#xa;第一种情况显然是远水救不了近火，网络故障都上不了网，何谈 Wiki。&#xa;因此，DocBook 能够实现的本地浏览，可打印的，有作者署名的电子文档，依然是 Wiki 不可替代的。"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_206098046" POSITION="right" 
+	TEXT="WHODO 的构成">
+<node ID="Freemind_Link_1565755613" 
+	TEXT="&lt;html&gt;&#xa;&lt;h2&gt;图示&lt;/h2&gt;&#xa;&lt;pre&gt;&#xa;     | &lt;--- [ Freemind ] &lt;---- *.mm          &lt;--下载-- |               | &lt;--------------------------- |     |&#xa;读者 |                                              | WHODO Website |                             | SVN |&#xa;     |                         *.html, *.chm        |               |                             |     |&#xa;     | &lt;--- [ IE, Word ] &lt;---- *.pdf,  *.rtf &lt;--下载-- |               | &lt;--- [ DocBook 编译系统 ] -- |     |&#xa;     |                         *.txt&#xa;     &#xa;     | &lt;--&gt; [ Freemind* ] &lt;----------------------&gt; *.mm  &lt;----&gt; |     |&#xa;作者 |                                                       | SVN |&#xa;     | &lt;--&gt; [ Notepad, Ultraedit, Eclipse...] &lt;--&gt; *.xml &lt;----&gt; |     |&#xa;     |                      &#xa;&lt;/pre&gt;"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_683054111" POSITION="right" 
+	TEXT="如何参与 WHODO？">
+<node ID="Freemind_Link_24001140" 
+	TEXT="WHODO 是人人皆可参与文档撰写平台。用户可以从读者身份做起，一步一步成为 WHODO 计划的贡献者。"/>
+<node FOLDED="true" ID="Freemind_Link_1891157461" 
+	TEXT="读者">
+<node ID="Freemind_Link_541481135" 
+	TEXT="作为读者，您可以从网站直接访问到 WHODO 文档，可以在线阅读，也可以下载到本地。">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node ID="Freemind_Link_1162073510" 
+	TEXT="WHODO 文档基于 GPL 版权，您可以传播给任何人，您可以在您的文章中任意引用，只要您指明文档的出处以及作者的有关声明。"/>
+<node ID="Freemind_Link_1055437023" 
+	TEXT="如果您读到对您有益的文章，如果您能够给作者发一封 Email，这将会鼓励作者写出更多、更好的文档。"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_822145193" 
+	TEXT="反馈者">
+<node FOLDED="true" ID="Freemind_Link_589177598" 
+	TEXT="通过问题报告反馈">
+<node ID="Freemind_Link_1807482865" 
+	TEXT="当您发现文档中的纰漏或者不足，您可以到问题报告系统上来，提交一个针对该文档的问题报告。"/>
+<node ID="Freemind_Link_1929880754" 
+	TEXT="或者您希望了解某方面内容，而文档中没有提及或不详细，您也可以通过问题报告系统提出您的需求，作者或者其它贡献者可能会很快加以完善。"/>
+<node ID="Freemind_Link_1223862034" 
+	TEXT="问题报告系统是对所有用户公开的，您只要到上面注册一个账号即可。"/>
+</node>
+<node ID="Freemind_Link_1019933685" 
+	TEXT="拥有 Wiki、论坛、SVN 版本控制系统的有限权限"/>
+<node FOLDED="true" ID="Freemind_Link_640770298" 
+	TEXT="Wiki">
+<node ID="Freemind_Link_748763319" 
+	TEXT="WHODO 的主页是用 Wiki 构建的，WHODO 的帮助、FAQ，甚至整个理念，都处于不断发展之中，欢迎您来丰富和完善。"/>
+<node ID="Freemind_Link_52811477" 
+	TEXT="Wiki 系统虽然是开放注册，但新注册用户只能够修改 Talk 页。"/>
+<node ID="Freemind_Link_1042225986" 
+	TEXT="只有通过 问题报告系统或者 Wiki 提供了对 WHODO 文章的反馈，便可申请获得“作者”身份，而拥有最大权限。"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_859293579" 
+	TEXT="论坛">
+<node ID="Freemind_Link_877120694" 
+	TEXT="论坛不对外开放，没有授权，无法访问。"/>
+<node ID="Freemind_Link_468036955" 
+	TEXT="只有通过 问题报告系统或者 Wiki 提供了对 WHODO 文章的反馈，便可申请获得“作者”身份，而拥有最大权限。"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_319100957" 
+	TEXT="SVN 版本控制系统">
+<node ID="Freemind_Link_1566241531" 
+	TEXT="SVN 版本控制系统，是 WHODO 作者修改和提交文章的支撑系统。普通用户只有只读权限。"/>
+<node ID="Freemind_Link_479279070" 
+	TEXT="只有通过 问题报告系统或者 Wiki 提供了对 WHODO 文章的反馈，便可申请获得“作者”身份，而拥有最大权限。"/>
+</node>
+</node>
+<node FOLDED="true" ID="Freemind_Link_79757662" 
+	TEXT="作者">
+<node FOLDED="true" ID="Freemind_Link_593125128" 
+	TEXT="Wiki">
+<node ID="Freemind_Link_1011714413" 
+	TEXT="可以对所有页面进行修改。"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_39110587" 
+	TEXT="论坛">
+<node ID="Freemind_Link_1217290047" 
+	TEXT="可以访问论坛"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_593519046" 
+	TEXT="SVN 版本控制系统">
+<node ID="Freemind_Link_1577756225" 
+	TEXT="可以修改 SVN 管理的文档和代码。"/>
+</node>
+</node>
+</node>
+<node FOLDED="true" ID="Freemind_Link_1743143715" POSITION="right" 
+	TEXT="WHODO 文档列表">
+<node ID="Freemind_Link_1933636883" 
+	TEXT="A-Z 文档列表"/>
+<node FOLDED="true" ID="Freemind_Link_1423746467" 
+	TEXT="文档分类列表">
+<node ID="Freemind_Link_111704061" 
+	TEXT="参见 WIKI 页面"/>
+</node>
+</node>
+<node FOLDED="true" ID="Freemind_Link_1040339879" POSITION="right" 
+	TEXT="WHODO 贡献者列表">
+<node ID="Freemind_Link_1537362939" 
+	TEXT="A-Z"/>
+<node ID="Freemind_Link_1195726348" 
+	TEXT="更多参见 Wiki 页面"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_1870376841" POSITION="right" 
+	TEXT="附录">
+<node FOLDED="true" ID="Freemind_Link_191946185" 
+	TEXT="WHODO 历史">
+<node ID="Freemind_Link_938636007" 
+	TEXT="http://www.worldhello.net 网站诞生于 2002 年底，最早是蒋鑫的个人网站，那时叫做 Johnson&apos;s Homepage"/>
+<node ID="Freemind_Link_1206794608" 
+	TEXT="那个时候，已经有了一年多 DocBook 使用经验的我，迫不及待的想借用个人网站的形式加以推广。 WorldHello 就这么诞生了。以 DocBook 进行文档的撰写和个人的知识总结，是 WorldHello 的特色，但那个时候，往好听了说是独自耕耘，其实就是自言自语。"/>
+<node ID="Freemind_Link_1917804181" 
+	TEXT="2004 年开始接触到 MindMap，直到后来发现了 Freemind 这个伟大的处理“思维导图”的开源软件，我的个人文档撰写和知识积累的习惯再一次被颠覆了。"/>
+<node ID="Freemind_Link_1869277930" 
+	TEXT="2005年中开始筹建公司，主要方向是“开源软件”的商业支持。忽然发现仍然有很多网友关心着我渐已荒废的 文档。为什么不开源呢？一方面将我荒废的文档交由感兴趣的作者继续，另外一方面，推广 Freemind + DocBook + CVS/SVN 的文档撰写模式呢？"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_1875149244" 
+	TEXT="Freemind (WorldHello Edition)">
+<node ID="Freemind_Link_1484670243" 
+	TEXT="UTF-8 以及更好的中文支持"/>
+<node ID="Freemind_Link_1327871183" 
+	TEXT="版本控制优化"/>
+<node ID="Freemind_Link_1067041278" 
+	TEXT=".mmx 文件和 .mm 合并算法需要优化"/>
+<node ID="Freemind_Link_1811271933" 
+	TEXT="参见 jiangxin@Freemind"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_839365805" 
+	TEXT="DocBook (WorldHello XSL)">
+<node ID="Freemind_Link_547289206" 
+	TEXT="如何部署 DocBook 编译环境"/>
+<node ID="Freemind_Link_265941213" 
+	TEXT="如何编译 WHODO 文档"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_37470138" 
+	TEXT="如何访问 SVN 版本控制系统？">
+<node ID="Freemind_Link_1829151789" 
+	TEXT="命令行"/>
+<node ID="Freemind_Link_162848162" 
+	TEXT="图形界面"/>
+</node>
+</node>
+</node>
+</map>
